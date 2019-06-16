552211444f72ecbf7384ab5135423a9668ede292
Johnathan Leon <jaleonro@unal.edu.co>
2019-06-15 22:42:13 -0500

    update1

--- src/excursions/typeDefs.js
+++ src/excursions/typeDefs.js
@@ -10,33 +10,33 @@ type Excursion {
     state: Int!
 }
 input ExcursionCreationInput {
-    name: String!
-    price: Int!
-    location: String!
-    description: String!
-    photo_path: String!
-    duration: Int!
+    name: String
+    price: Int
+    location: String
+    description: String
+    photo_path: String
+    duration: Int
 }
 input ExcursionEditionInput {
-    name: String!
-    price: Int!
-    location: String!
-    description: String!
-    photo_path: String!
-    duration: Int!
-    state: Int!
+    name: String
+    price: Int
+    location: String
+    description: String
+    photo_path: String
+    duration: Int
+    state: Int
 }`;
 
 export const excursionsQueries = `
-    allExcursions: [Excursion]!
-    excursionById(id: Int!): Excursion!
-    excursionByDuration(duration: Int!): [Excursion]!
-    excursionByLocation(location: String!): [Excursion]!
-    excursionByPrice(price: Int!): [Excursion]!
+    allExcursions: [Excursion]
+    excursionById(id: Int!): Excursion
+    excursionByDuration(duration: Int!): [Excursion]
+    excursionByLocation(location: String!): [Excursion]
+    excursionByPrice(price: Int!): [Excursion]
 `;
 
 export const excursionsMutations = `
-    createExcursion(excursion: ExcursionCreationInput!): Excursion!
+    createExcursion(excursion: ExcursionCreationInput!): Excursion
     deleteExcursion(id: Int!): Int
-    updateExcursion(id: Int!, excursion: ExcursionEditionInput!): Excursion!
+    updateExcursion(id: Int!, excursion: ExcursionEditionInput!): Excursion
 `;
