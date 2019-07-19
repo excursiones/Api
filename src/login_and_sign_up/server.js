2388b2cf5b20c33518d6458cbca98b233fd587ac
Fabio Tovar <fstovarr@unal.edu.co>
2019-07-19 04:46:21 -0500

    Recompile

--- src/login_and_sign_up/server.js
+++ src/login_and_sign_up/server.js
@@ -1,4 +1,4 @@
 export const url = "web6.authorization-ms"
 export const port = 3002
-export const entryPoint = process.env.LOGIN_ENTRY || "/sign_in"
-export const signUpEntryPoint = process.env.SIGN_UP_ENTRY || "/sign_up"
+export const entryPoint = process.env.LOGIN_ENTRY || "sign_in"
+export const signUpEntryPoint = process.env.SIGN_UP_ENTRY || "sign_up"
